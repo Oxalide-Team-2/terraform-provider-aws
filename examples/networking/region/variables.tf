852186d21d44248d9ff346759c884b202965d991
Julien Boulanger <julien.boulanger@oxalide.com>
2017-07-24 00:26:53 +0200

    break things for exercises

--- examples/networking/region/variables.tf
+++ examples/networking/region/variables.tf
@@ -2,6 +2,7 @@ variable "region" {
   description = "The name of the AWS region to set up a network within"
 }
 
+# Make this variable mandatory for this module
 variable "base_cidr_block" {}
 
 # provider "aws" {
